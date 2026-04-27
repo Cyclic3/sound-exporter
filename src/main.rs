e0ca74a404f061cd2775d6120ac0363f567f6196
Cyclic3 <cyclic3.git@gmail.com>
2026-04-27 17:00:41 +0100

    Updated with proper argument parsing

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,23 @@ use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::Router;
+use clap::Parser;
 use std::sync::Arc;
 
 use crate::audio::AudioProcessor;
 
 mod lufs;
 mod audio;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short('b'), long("bind"), default_value_t={"0.0.0.0:9011".to_string()})]
+    /// The endpoint to bind onto
+    bind_address: String,
+    #[arg(short('d'), long("device"))]
+    /// The device id for the microphone to monitor. Defaults to the system's default input.
+    input_device: Option<String>
+}
 
 pub async fn metrics_handler(State(processor): State<Arc<AudioProcessor>>) -> impl IntoResponse {
     let snapshot = processor.snapshot();
@@ -37,12 +48,12 @@ sound_short_term {short_term}
 
 #[tokio::main]
 async fn main() {
-    let processor = AudioProcessor::new(std::env::args().nth(1));
+    let args = Args::parse();
+    let processor = AudioProcessor::new(args.input_device);
     let router = Router::new()
         .route("/metrics", get(metrics_handler))
         .with_state(Arc::new(processor));
-    let port = 9011;
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+    let listener = tokio::net::TcpListener::bind(args.bind_address)
         .await
         .unwrap();
 
